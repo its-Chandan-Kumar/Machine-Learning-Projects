d7219767ca14834b3d9de3a3695fcd174a3a4425
Chandan Kumar <125624674+its-Chandan-Kumar@users.noreply.github.com>
2026-03-28 20:29:13 +0530

    Add files via upload

---
+++ Medical Insurance Cost Prediction/app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+from typing import Literal, Annotated
+import pandas as pd
+import joblib
+
+model = joblib.load("model.pkl")
+
+app = FastAPI()
+
+class UserInput(BaseModel):
+
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    sex: Annotated[Literal['male','female'], Field(..., description='Gender of the user')]
+    bmi: Annotated[float, Field(..., gt=0, lt=50, description='BMI of the user')]
+    children: Annotated[int, Field(..., ge=0, lt=10, description='Children of the user')]
+    smoker: Annotated[Literal['yes','no'], Field(..., description='Is the user Smoker')]
+    region: Annotated[Literal['southwest','southeast','northwest','northeast'], Field(...,description='Region on the user')]
+
+
+@app.get('/')
+def home():
+    return{'message': 'Medical Insurance Price Predictor'}
+
+@app.post('/predict')
+def predict_premium(data: UserInput):
+
+    sex = 1 if data.sex == "male" else 0
+    smoker = 1 if data.smoker == "yes" else 0
+
+    region_map = {
+        "southwest":0,
+        "southeast":1,
+        "northwest":2,
+        "northeast":3
+    }
+
+    input_df = pd.DataFrame([{
+        "age": data.age,
+        "sex": sex,
+        "bmi": data.bmi,
+        "children": data.children,
+        "smoker": smoker,
+        "region": region_map[data.region]
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    return {"Predicted Insurance Cost": float(prediction)}
+
+    # return JSONResponse(status_code=200, content={'Predicted Category':prediction})
---
+++ Medical Insurance Cost Prediction/frontend.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+
+API_URL = "http://<YOUR-EC2-IP>:8000/predict"
+
+st.title("Medical Insurance Price Predictor")
+st.markdown('Please provide your details below: ')
+
+# Input Fields
+age = st.number_input('Age', min_value=1, max_value=120, value=25)
+sex = st.selectbox('Gender', options=['male', 'female'])
+bmi = st.number_input("BMI", min_value=15.0, max_value=40.0,value=20.0)
+children = st.select_slider('Children',options=list(range(0, 11)),value=2)
+smoker = st.selectbox("Smoker",options=['yes','no'])
+region = st.selectbox("Region",options=['southeast','southwest','northeast','northwest'])
+
+
+
+if st.button("Predict Medical Insurance"):
+    input_data = {
+        'age':age,
+        'sex':sex,
+        'bmi':bmi,
+        'children':children,
+        'smoker':smoker,
+        'region':region
+    }
+
+    try:
+        response = requests.post(API_URL, json=input_data)
+        if response.status_code==200:
+            result = response.json()
+            st.success(f"Your Predicted Medical Insurance Price: ₹{result['Predicted Insurance Cost']:.2f}")
+        else:
+            st.error(f'API Error: {response.status_code} - {response.text}')
+    except requests.exceptions.ConnectionError:
+        st.error("Could not connect to the FastAPI server. Make sure it's running on port 8000")
